3f74c6b0f3d8226a9d26ac3c52941a2fd463eb17
Jeanne Margaret L. Guzon <guzonjeanne@gmail.com>
2024-02-23 11:12:59 +0800

    Added portfolio

--- js/main.js
+++ js/main.js
@@ -19,4 +19,26 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.removeChild(downloadLink);
     })
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('download-portfolio').addEventListener('click', function() {
+        var resumeUrl = '/media/Portfolio.pdf';
+        
+        // Create a temporary anchor element
+        var downloadLink = document.createElement('a');
+        downloadLink.href = resumeUrl;
+        downloadLink.download = 'Jeanne_Margaret_Guzon_Portfolio.pdf';
+        downloadLink.target = '_blank'; // Open in a new tab
+        downloadLink.style.display = 'none'; // Hide the link
+        
+        // Append the anchor element to the body
+        document.body.appendChild(downloadLink);
+        
+        // Trigger the click event
+        downloadLink.click();
+        
+        // Remove the anchor element
+        document.body.removeChild(downloadLink);
+    })
+});
     
